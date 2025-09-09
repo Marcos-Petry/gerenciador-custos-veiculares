85a57c472e751a99decfbc93e4c11d4425467a1a
Marcos-Petry <marcospetryjunior321@gmail.com>
2025-09-09 13:17:32 -0300

    alterações tela painel gerencial

--- lang/pt_BR/validation.php
+++ lang/pt_BR/validation.php
@@ -1,13 +1,23 @@
 <?php
 
 return [
-    'required' => 'O campo :attribute é obrigatório.',
-    'email'    => 'O campo :attribute deve ser um endereço de e-mail válido.',
-    'min'      => [
+    'required'   => 'O campo :attribute é obrigatório.',
+    'email'      => 'O campo :attribute deve ser um endereço de e-mail válido.',
+    'min'        => [
         'string' => 'O campo :attribute deve ter pelo menos :min caracteres.',
     ],
-    'max'      => [
+    'max'        => [
         'string' => 'O campo :attribute não pode ter mais de :max caracteres.',
     ],
-    'confirmed' => 'A confirmação do campo :attribute não confere.',
+    'confirmed'  => 'A confirmação do campo :attribute não confere.',
+    'unique'     => 'Este :attribute já está em uso.',
+
+    // Tradução dos nomes dos atributos
+    'attributes' => [
+        'email'    => 'E-mail',
+        'name'     => 'Nome',
+        'lastname' => 'Sobrenome',
+        'phone'    => 'Telefone',
+        'password' => 'Senha',
+    ],
 ];
