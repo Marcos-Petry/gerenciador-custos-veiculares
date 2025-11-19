9e1f9756f3b2717f5dfa8d7d5abf295aadbf09b4
Marcos-Petry <marcospetryjunior321@gmail.com>
2025-11-19 15:15:01 -0300

    ajuste placeholder

--- resources/views/gasto/create.blade.php
+++ resources/views/gasto/create.blade.php
@@ -44,8 +44,7 @@
         <div>
             <label for="descricao" class="block font-semibold text-gray-700 mb-1">Descrição</label>
             <input type="text" name="descricao" id="descricao"
-                class="w-full border rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                placeholder="Ex: Troca de óleo">
+                class="w-full border rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none">
         </div>
 
         <!-- Valor -->
